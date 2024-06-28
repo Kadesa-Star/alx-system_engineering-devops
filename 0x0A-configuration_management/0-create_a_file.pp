425c754cdb6d6d6e7399713fab6368d602cb05f0
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-28 21:16:59 +0300

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This manifest ensures that the file /tmp/school is created with the specified attributes
+
+
+file {  '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
