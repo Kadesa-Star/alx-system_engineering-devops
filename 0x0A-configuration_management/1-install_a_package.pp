01eed24a7af6f7e24bf6f093e0ffb7a3e3dec093
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-28 21:53:48 +0300

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-# This manifest ensures that Flask version 2.1.0 is installed using pip3
+# This manifest ensures that Flask version 2.1.0 and Werkzeug version 2.0.3 are installed using pip3
 
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
 
-package {  'puppet-lint':
-   ensure    => '2.1.1',
-   provider  => 'gem',
+package { 'Werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
 }
