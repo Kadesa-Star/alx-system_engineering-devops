813ae63a475d0a499dd833159ded88787548000a
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-30 21:49:02 +0300

    client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,5 @@
-# Ensure SSH client configuration file exists
 file { '/home/ubuntu/.ssh/config':
   ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
-  content => "\
-# SSH client configuration managed by Puppet
-
-Host *
-    PasswordAuthentication no
-    IdentityFile ~/.ssh/school
-",
+  mode    => '0600',
+  source  => '/root/alx-system_engineering-devops/0x0B-ssh/client_config.erb',
 }
