6e4d59d8bc49b1c579ccc4a194fb4940d7c200d4
Kadesa <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-30 22:11:14 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,15 @@
-# Puppet script to create ssh config file
-include stdlib
+# Puppet script to configure SSH client settings for passwordless authentication
 
-file_line { 'Turn off passwd auth and declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => [
-    '    PasswordAuthentication no',
-    '    IdentityFile ~/.ssh/school',
-  ],
+# Ensure that PasswordAuthentication is set to 'no' in the SSH client configuration
+file_line { 'Turn off passwd auth':
+  ensure => 'present',                  # Ensure the line exists in the file
+  path   => '/etc/ssh/ssh_config',      # Path to the SSH client configuration file
+  line   => 'PasswordAuthentication no', # Line to add or modify in the file
+}
+
+# Ensure that IdentityFile is set to '~/.ssh/holberton' in the SSH client configuration
+file_line { 'Declare identity file':
+  ensure => 'present',                  # Ensure the line exists in the file
+  path   => '/etc/ssh/ssh_config',      # Path to the SSH client configuration file
+  line   => 'IdentityFile ~/.ssh/holberton', # Line to add or modify in the file
 }
