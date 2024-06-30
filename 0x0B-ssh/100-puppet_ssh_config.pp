87699b36b4bac2f160a9c78b156e491dae8c9aca
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-30 21:50:07 +0300

    client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,31 @@
+# Puppet script to create SSH config file
+
+# Ensure the ~/.ssh directory exists with correct permissions
+file { '/home/ubuntu/.ssh':
+  ensure  => 'directory',
+  mode    => '0700',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+# Create or update the SSH config file
 file { '/home/ubuntu/.ssh/config':
-  ensure  => present,
+  ensure  => 'file',
   mode    => '0600',
-  source  => '/root/alx-system_engineering-devops/0x0B-ssh/client_config.erb',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => template('alx-system_engineering-devops/0x0B-ssh/client_config.erb'),
+}
+
+# Ensure SSH client configuration settings are applied
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
