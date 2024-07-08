6f6e160472a56bd2aba39ebe5ed2c8b3923e8f85
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-08 22:05:27 +0300

    add a custom http header with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,56 +1,30 @@
 # 2-puppet_custom_http_response_header.pp
-# This Puppet manifest configures Nginx to include a custom HTTP header 'X-Served-By' with the hostname of the server
+# This Puppet manifest automates configuration of Nginx to include a custom HTTP header 'X-Served-By' with the server's hostname
 
-node default {
-  
-  # Ensure the package 'nginx' is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure the Nginx service is running and enabled at boot
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
-
-  # Manage the Nginx configuration file to include the custom HTTP header
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Update package list
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
-# Create the template file 'default.erb'
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => @(EOF)
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name _;
-    root /var/www/html;
-
-    # Add custom HTTP header
-    add_header X-Served-By <%= @hostname %>;
-
-    index index.html index.htm index.nginx-debian.html;
-    location / {
-        try_files $uri $uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=xJJsoquu70o;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
+# Ensure nginx package is present
+package { 'nginx':
+  ensure => present,
+  require => Exec['update'],
 }
-  | EOF
-  require => File['/etc/nginx/sites-available/default'],
+
+# Add custom HTTP header to nginx configuration
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => '^http {',
+  line  => "http {\n    add_header X-Served-By \"${hostname}\";",
+  require => Package['nginx'],
+}
+
+# Restart nginx service to apply changes
+exec { 'run':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+  subscribe   => File_line['http_header'],
+  path        => ['/usr/bin', '/usr/sbin'],
 }
