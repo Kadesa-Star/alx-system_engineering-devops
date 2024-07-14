c2d8ae122f71b0b49bf1e037cbf191032fbdc180
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-14 14:30:29 +0300

    install nginx with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,31 +1,27 @@
-# Configure a new Ubuntu server with Nginx using Puppet
+# Setup New Ubuntu server with nginx
 
-# Update the system's package list
-exec { 'update_system':
-  command => '/usr/bin/apt-get update',
+exec { 'update system':
+    command => '/usr/bin/apt-get update',
 }
 
-# Install the Nginx package
 package { 'nginx':
-  ensure   => installed,
-  require  => Exec['update_system'],
+    ensure => 'installed',
+    require => Exec['update system'],
 }
 
-# Create an index.html file with "Hello World!" content
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+    ensure => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
 }
 
-# Add a redirection rule to the Nginx configuration for /redirect_me
-exec { 'add_redirect':
-  command  => 'sed -i "24i\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-  require  => Package['nginx'],
+exec { 'redirect_me':
+    command => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell',
+    require => Package['nginx'],
 }
 
-# Ensure the Nginx service is running
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+    ensure => running,
+    require => Package['nginx'],
 }
