e1424dd4fdf127a03cacab8ac54c39984c0c9963
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-13 21:29:35 +0300

    how many subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-Script that queries the number of subscribers
-for a given Reddit subreddit.
+Script queries number of subscribers for a given subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the total number of subscribers for a given subreddit.
-    """
+    """ Returns total number of subscribers for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {
-            'User-Agent': 'RedditSubChecker/1.0 (Kadesa)'
-        }
+    headers = {'User-Agent': 'Google Chrome Version 127.0.6533.100'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
+        return data.get('data', {}).get('subscribers')
     else:
         return 0
