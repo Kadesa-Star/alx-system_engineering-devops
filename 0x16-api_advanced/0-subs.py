d65e918fbfbb5969b5c9f0c7365a5e7273ff0d62
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-14 00:06:58 +0300

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@ Module for querying Reddit API to get the number of subscribers
 for a subreddit.
 """
 
+from requests import get
 import requests
 
 
@@ -12,6 +13,9 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API.
     - If not a valid subreddit, return 0.
     """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Google Chrome Version 127.0.6533.100"
