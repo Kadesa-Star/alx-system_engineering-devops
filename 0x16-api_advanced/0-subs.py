3e8c8ddf38bbcb7b77bebe466be8348896f16019
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-13 22:29:24 +0300

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """
-Script queries number of subscribers for a given subreddit.
+Module querying Reddit API to get no of subscribers for subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns total number of subscribers for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Google Chrome Version 127.0.6533.100'}
+    """
+    Queries Reddit API to return no of subscribers for a subreddit.
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data', {}).get('subscribers')
-    else:
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit,
+        or 0 if invalid or error.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (compatible; MyRedditAgent/1.0)'
+    }
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False
+        )
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
