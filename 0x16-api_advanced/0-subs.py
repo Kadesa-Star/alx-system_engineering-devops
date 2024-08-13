638ffba560a28b2a1750d5ec3d6f6f2ced6436c5
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-13 20:34:44 +0300

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,14 @@ def number_of_subscribers(subreddit):
     Returns:
         int: Number of subscribers or 0 if the subreddit is invalid.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'myapi/0.0.1'}
+    url = f'http://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'RedditSubCheker/1.0 (kadesa task 0)'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
         else:
             return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except Exception:
         return 0
