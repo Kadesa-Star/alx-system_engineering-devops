c3b2f7e499da4a263751d126da9a03f1c3222e42
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-13 20:50:38 +0300

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,14 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns the total number of subscribers for a given subreddit.
+
     Args:
         subreddit (str): The name of the subreddit.
+
     Returns:
         int: No of subscribers,0 if subreddit is invalid or err.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (compatible; RedditSubChecker/1.0)"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
