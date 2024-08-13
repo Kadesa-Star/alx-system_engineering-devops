0d1f176510240ce44ef64da92223b4bdb02e809e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-13 20:37:18 +0300

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit.
+    """
+    Returns the number of subscribers for a given subreddit.
     Args:
         subreddit (str): The name of the subreddit.
     Returns:
         int: Number of subscribers or 0 if the subreddit is invalid.
     """
-    url = f'http://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'RedditSubCheker/1.0 (kadesa task 0)'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'RedditSubChecker/1.0 (kadesa task 0)'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -19,5 +20,5 @@ def number_of_subscribers(subreddit):
             return data.get('subscribers', 0)
         else:
             return 0
-    except Exception:
+    except requests.RequestException:
         return 0
