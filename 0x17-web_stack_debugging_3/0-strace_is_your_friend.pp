9786ac33ade6eb57ee7fe5c6f46f73d3582ad7ee
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-18 22:18:37 +0300

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-exec { 'fix-wordpress':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin/:/bin/',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
-  notify      => Service['apache2'],
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  require    => Exec['fix-wordpress'],
+# Using strace and then using Puppet to automate the fix for 500 error on Apache
+exec { 'fix_typo':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  provider => 'shell',
 }
