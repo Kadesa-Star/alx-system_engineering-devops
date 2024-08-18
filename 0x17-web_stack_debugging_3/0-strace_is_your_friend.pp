62b03fe9fd13f8da7e1d288e9bb28de1e69a1286
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-18 22:12:13 +0300

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,15 @@
-# This manifest corrects the permissions of the WordPress directory
-file { '/var/www/html/wordpress':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+exec { 'fix-wordpress':
+  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path        => '/usr/local/bin/:/bin/',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
+  notify      => Service['apache2'],
 }
 
+# Ensure Apache service is running
 service { 'apache2':
-  ensure => running,
+  ensure     => running,
+  enable     => true,
+  require    => Exec['fix-wordpress'],
 }
