5930b118bbe68117626acaefc288248a2d4e17d4
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-22 04:07:01 -0700

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,8 @@
-# This Puppet manifest increases the max open files limit for Nginx and restarts the service.
+# This Puppet manifest fixes Nginx to accept and serve more requests by modifying the max open files limit and restarting the Nginx service.
 
-# Increase the max open files limit for Nginx
+# Increase the max open files limit for Nginx and restart the service
 exec { 'modify-max-open-files-limit':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx',
-  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  notify  => Service['nginx'],  # Restart Nginx if this command modifies the file
-}
-
-# Ensure Nginx is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  subscribe => Exec['modify-max-open-files-limit'],  # Restart if the file is modified
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  unless  => 'grep "10000" /etc/default/nginx',  # Check if the line is already set to 10000
 }
