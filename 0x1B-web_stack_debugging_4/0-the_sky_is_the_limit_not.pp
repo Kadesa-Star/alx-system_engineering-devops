bce794df0c3da509276c8f7b308ffa3367c6a3e5
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-22 03:05:13 -0700

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,15 @@
 # This Puppet manifest increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+exec { 'increase-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => ['/usr/local/bin', '/bin'],
+  notify  => Service['nginx'], # Ensure Nginx is restarted if this changes
+}
 
 # Restart Nginx
-exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  subscribe => Exec['increase-nginx-ulimit'], # Restart if the file is changed
 }
