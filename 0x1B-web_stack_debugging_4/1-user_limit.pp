3f197e956626c2398c34dda6689ce221aa27e63b
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-22 04:09:56 -0700

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,16 @@
-# Enable the user holberton to login and open files without error.
+# This Puppet manifest increases the hard and soft file limits for the holberton user
+# to prevent "Too many open files" errors.
 
-# Increase hard file limit for Holberton user.
+# Increase hard file limit for Holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton hard/s/^[0-9]*$/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton hard nofile/s/^[0-9]*$/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin:/bin',
-  unless  => 'grep -q "^holberton hard.*50000" /etc/security/limits.conf',
+  unless  => 'grep -q "^holberton hard nofile 50000" /etc/security/limits.conf',
 }
 
-# Increase soft file limit for Holberton user.
+# Increase soft file limit for Holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/^[0-9]*$/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton soft nofile/s/^[0-9]*$/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin:/bin',
-  unless  => 'grep -q "^holberton soft.*50000" /etc/security/limits.conf',
+  unless  => 'grep -q "^holberton soft nofile 50000" /etc/security/limits.conf',
 }
